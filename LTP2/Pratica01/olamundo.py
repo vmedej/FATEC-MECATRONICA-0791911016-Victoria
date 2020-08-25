fb35e2cf69646ad4ccf57ebde4cd025ac42de120
vmedej <70210745+vmedej@users.noreply.github.com>
2020-08-25 10:29:36 -0300

    Update olamundo.py

--- LTP2/Pratica01/olamundo.py
+++ LTP2/Pratica01/olamundo.py
@@ -1,6 +1,1 @@
-print ("ola mundo")
-
-numero1 = int (input ("informe um numero: "))
-numero2 = int (input ("informe outro numero: "))
-soma = numero1+numero2
-print ("o resultado e: ", soma)
+print ("olá mundo")
