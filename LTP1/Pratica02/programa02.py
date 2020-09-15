2a65992da1bae44a4f8839418e070797c00a0873
vmedej <70210745+vmedej@users.noreply.github.com>
2020-09-15 08:36:13 -0300

    Create programa02.py

---
+++ LTP1/Pratica02/programa02.py
@@ -0,0 +1,14 @@
+#Cria uma variavel para guardar a senha_secreta
+senha_secreta = "123456"
+
+#Pede ao usuário uma senha e a valida com a senha secreta
+#Cria uma variavel para guardar a senha que o usuário digita
+senha = input("Informe sua senha: ")
+
+#Verifica se a senha do usuário esta compile
+
+if senha == senha_secreta:
+  print("Bem vindo")
+
+else:
+  print ("Senha incorreta")
