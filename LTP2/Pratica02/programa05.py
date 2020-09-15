5084afd398927f93378fec5de58a09b899aaceb1
vmedej <70210745+vmedej@users.noreply.github.com>
2020-09-14 23:38:40 -0300

    Create programa05.py

---
+++ LTP2/Pratica02/programa05.py
@@ -0,0 +1,15 @@
+#Determinar se um número inteiro é par ou impar
+
+numero = int(input("Digite um numero: "))
+
+#Calcula o resto da divisao do número por 2
+
+resto = numero%2
+
+#Condição
+
+if resto == 0:
+  print("\nO número é par")
+  print("Vencedor")
+  
+print("\nEsse sempre aparece!")
