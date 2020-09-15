f96e1798efeee266390313b45e925b61121b41da
vmedej <70210745+vmedej@users.noreply.github.com>
2020-09-14 23:55:21 -0300

    Create programa01.py

---
+++ LTP2/Pratica03/programa01.py
@@ -0,0 +1,15 @@
+#Quanto utilizadao =, é uma ordem de atribuição
+
+numero_secreto = 32
+
+palpite = int(input("Informe um palpite: "))
+
+#Quando utilizamos ==, estamos fazendo uma pergunta
+
+if palpite == numero_secreto:
+  print("Parabéns, você adivinhou!")
+else:
+  if palpite > numero_secreto:
+    print("Chute um número menor!")
+  else:
+    print("Chute um número maior!")
