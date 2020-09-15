15ebac58280e8c4d914c44965b8b786104b0265c
vmedej <70210745+vmedej@users.noreply.github.com>
2020-09-15 00:28:53 -0300

    Create programa07.py

---
+++ LTP2/Pratica03/programa07.py
@@ -0,0 +1,52 @@
+#Para limpar a tela
+from os import system
+from math import sin, cos, radians
+
+#Nossa calculadora
+
+#Cria uma função
+def mostrar_menu():
+  system("clear")
+  print("0 - Sair")
+  print("+ - Somar")
+  print("- - Subtrair")
+  print("* - Multiplicar")
+  print("/ - Dividir")
+  print("sen - Seno")
+  print("cos - Cosseno")
+  print("elevado - Potência")
+  print("raiz - Calcular a Raiz")
+
+def somar():
+  numero1 = float(input("Número 1: "))
+  numero2 = float(input("Número 2: "))
+  resultado = numero1 + numero2
+  print("O resultado da soma é:", resultado)
+
+def subtrair():
+  numero1 = float(input("Número 1: "))
+  numero2 = float(input("Número 2: "))
+  resultado = numero1 - numero2
+  print("O resultado da subtração é:", resultado)
+
+def seno():
+  angulo = float(input("Angulo: "))
+  print("O seno do ângulo é:", sin(radians(angulo)))
+
+ligado = True
+
+while ligado == True:
+  mostrar_menu()
+  opcao = input("Opção: ")
+  if opcao == '0':
+    ligado = False
+  elif opcao == '+':
+    somar()
+  elif opcao == '-':
+    subtrair()
+  elif opcao == 'sen':
+    seno()
+  
+  input("Pressione enter para continuar...")
+
+print("Até logo!")
