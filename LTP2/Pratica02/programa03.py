5010439fcc2d4386cec143d4679a793be5a27937
vmedej <70210745+vmedej@users.noreply.github.com>
2020-09-14 23:30:00 -0300

    Create programa03.py

---
+++ LTP2/Pratica02/programa03.py
@@ -0,0 +1,6 @@
+#O programa calcula a média entre dois números
+
+numero1 = int(input("numero 1: "))
+numero2 = int(input("numero 2: "))
+media = (numero1+numero2)/2
+print("o resultado da media eh:", media)
