1a1922aa0977f09991f1221ac6ba2cee09b1bd88
vmedej <70210745+vmedej@users.noreply.github.com>
2020-09-15 08:04:07 -0300

    Create programa01.py

---
+++ LTP1/Pratica02/programa01.py
@@ -0,0 +1,9 @@
+#Pede a um usuário para digitar a sua idade
+idade = int(input("Informe a sua idade: "))
+
+#Pergunte, no código, se a idade é maior ou igual a 18
+if idade >= 18:
+  print ("Você possui 18 anos ou mais")
+  print ("Agora você pode dirigir")
+
+print ("Final do programa!")
