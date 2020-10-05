03088b4e648cfaaeb9bec57e73d4e81c8bf2c878
vmedej <70210745+vmedej@users.noreply.github.com>
2020-10-05 20:49:34 -0300

    Create programa04.py

---
+++ LTP1/Pratica04/programa04.py
@@ -0,0 +1,12 @@
+#Estruturas de decisão
+
+temperatura_limite = input("Informe uma temperatura limite: ")
+temperatura_atual = input("Informe a temperatura atual: ")
+
+if temperatura_atual > temperatura_limite:
+  print("A temperatura está acima do limite!")
+  
+if temperatura_atual < temperatura_limite:
+  print("A temperatura está abaixo do limite!")
+
+print("Obrigada por utilizar o sistema!")
