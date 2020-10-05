65f49e9d10f8493a9f623e9f8816583b554cd038
vmedej <70210745+vmedej@users.noreply.github.com>
2020-10-05 20:40:32 -0300

    Create programa03.py

---
+++ LTP1/Pratica04/programa03.py
@@ -0,0 +1,13 @@
+#Operações matemáticas
+
+numero_1 = float(input("Digite o primeiro valor: "))
+numero_2 = float(input("Digite o segundo valor: "))
+
+print("Soma:", numero_1+numero_2)
+print("Subtração:", numero_1-numero_2)
+print("Multiplicação:", numero_1*numero_2)
+print("Divisão:", numero_1/numero_2)
+print("Divisão inteira:", numero_1//numero_2)
+print("Resto da divisão:", numero_1%numero_2)
+print("Potência:", numero_1**numero_2)
+print("Raiz:", numero_1**(1./numero_2))
