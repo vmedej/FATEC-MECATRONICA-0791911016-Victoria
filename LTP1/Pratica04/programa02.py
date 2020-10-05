f0c42141149818f691835e6f0bb7a3e4f98556bc
vmedej <70210745+vmedej@users.noreply.github.com>
2020-10-05 20:34:14 -0300

    Create programa02.py

---
+++ LTP1/Pratica04/programa02.py
@@ -0,0 +1,8 @@
+#Ler dois números informados pelo usuário e apresentar a soma deles
+
+numero_1 = int(input("Digite o primeiro valor: "))
+numero_2 = float(input("Digite o segundo valor: "))
+
+resultado = numero_1 + numero_2
+
+print("O resultado é:", resultado)
