18ee2585fe750adc9e56c6d92ba4c0eb8e2a2586
vmedej <70210745+vmedej@users.noreply.github.com>
2020-10-29 18:56:38 -0300

    Create questão05.py

---
+++ LTP1/P1/questão05.py
@@ -0,0 +1,16 @@
+#Programa
+ra = int(input("Informe os dois últimos digitos do seu RA: "))
+if ra % 2 == 0:
+  valor = ra // 2
+else:
+  valor = ra / 2
+
+valor = valor ** 2 + ra % 3 - 0.01 ** 4.67
+
+if valor > 0:
+  valor = 3 ** (5-2)
+
+print(valor)
+
+#Resposta
+27
