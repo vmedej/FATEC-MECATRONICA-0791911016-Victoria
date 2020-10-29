87545dada5dc10167f00b87b94651b289d3a520a
vmedej <70210745+vmedej@users.noreply.github.com>
2020-10-29 19:42:39 -0300

    Create programa02.py

---
+++ LTP1/Pratica05/programa02.py
@@ -0,0 +1,33 @@
+#Guarda a somatoria dos valores informados
+somatoria = 0
+#Conta a quantidade de valores informados
+contador = 0
+#Guarda o maior valor informado
+maior = 0
+#Guarda o menor valor informado
+menor = 0
+#Constrói a lógica de repetição - enquanto se mantiver verdadeiro, repete o código
+continuar = True
+
+while continuar == True:
+  valor = int(input("Informe o valor: "))
+  #Adiciona o valor na somatoria
+  somatoria += valor #somatoria = somatoria + valor
+  #Adiciona mais um na contagem
+  contador += 1 #contador = contador + 1
+  #Para verificar se é  primeiro número informado
+  if contador == 1:
+    maior = valor
+    menor = valor
+  else:
+    if valor > maior:
+      maior = valor
+    elif valor < menor:
+      menor = valor
+  #Verifica se o usuário deseja continuar
+  continuar = input("Continuar? ") == 'sim'
+
+media = somatoria/contador
+print("Média:", media)
+print("Maior valor:", maior)
+print("Menor valor:", menor)
