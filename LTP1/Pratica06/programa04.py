6563c8a01b64521749cb3a2f4ed47de12941048a
vmedej <70210745+vmedej@users.noreply.github.com>
2020-11-12 20:32:04 -0300

    Create programa04.py

---
+++ LTP1/Pratica06/programa04.py
@@ -0,0 +1,12 @@
+#Len conta a quantidade de letras da marca, len conta espaço também
+marca = "Nintendo"
+quantidade = len(marca)
+
+print(marca)
+print(quantidade)
+
+marca = "Sony"
+quantidade = len(marca)
+
+print(marca)
+print(quantidade)
