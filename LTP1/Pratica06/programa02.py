1b59a8543d43e7d786bcc4e11b3747d83f8052c3
vmedej <70210745+vmedej@users.noreply.github.com>
2020-11-12 20:18:50 -0300

    Create programa02.py

---
+++ LTP1/Pratica06/programa02.py
@@ -0,0 +1,18 @@
+#Fatiamento de string
+palavra = "Victoria Medej"
+
+#Para mostrar apenas uma letra da palavra (Ver a posição da letra)
+letra = palavra[3]
+print(letra)
+
+#Para mostrar um conjuto de letras de uma string
+letras = palavra[0:5]
+print(letras)
+
+#Para mostrar da posição escolhida até o final
+letras = palavra[5:]
+print(letras)
+
+#Para mostrar da posição escolhida até o começo
+letras = palavra[:11]
+print(letras)
