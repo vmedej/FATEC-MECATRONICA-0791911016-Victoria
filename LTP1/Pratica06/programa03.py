0628187245bbca5b389104ebed77eb3152a2f893
vmedej <70210745+vmedej@users.noreply.github.com>
2020-11-12 20:27:40 -0300

    Create programa03.py

---
+++ LTP1/Pratica06/programa03.py
@@ -0,0 +1,9 @@
+#Texto
+#FUNCIONA - Porque da pra acessar índice de texto
+valor = "349.99"
+print(valor [0:3])
+
+#Float
+#NÃO FUNCIONA - Porque float não tem índice
+valor_numerico = float(valor)
+print(valor_numerico[0:3])
