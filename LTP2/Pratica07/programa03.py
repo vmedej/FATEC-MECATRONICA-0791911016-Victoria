ba02ef509f1136605802e0fe72c62769956232a7
vmedej <70210745+vmedej@users.noreply.github.com>
2020-11-25 22:17:50 -0300

    Create programa03.py

---
+++ LTP2/Pratica07/programa03.py
@@ -0,0 +1,11 @@
+#Leitura dos dados
+nome = input("Informe um nome: ")
+poder = input("Informe um poder: ")
+historia_origem = input("Informe a história de origem: ")
+universo = input("Informe qual é o universo: ")
+
+#Possibilidade 1
+heroi = {"nome": nome, "poder": poder, "historia_origem": historia_origem, "universo": universo}
+
+print("Criado pelo método 1: ")
+print(heroi)
