31996f9ee8691fffd58e2ca4e909a032187b6bbf
vmedej <70210745+vmedej@users.noreply.github.com>
2020-11-25 22:39:10 -0300

    Create programa01.py

---
+++ LTP2/Pratica08/programa01.py
@@ -0,0 +1,14 @@
+gastos = {}
+
+continuar = True
+while continuar == True:
+  valor_do_gasto = float(input("Informe o valor: "))
+  categoria = input("Informe a categoria: ").lower()
+  #Verificando se a categoria já existe no dicionário
+  if categoria in gastos.keys():
+    gastos[categoria] += valor_do_gasto
+  else:
+  #Uma nova categoria será criada
+   gastos[categoria] = valor_do_gasto
+  print(gastos)
+  continuar = input("Deseja continuar? ").lower()  == "sim"
