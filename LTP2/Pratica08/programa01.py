2abb5cd72d80a8c4898a4dfe6796c134b524988e
vmedej <70210745+vmedej@users.noreply.github.com>
2020-11-25 22:45:33 -0300

    Update programa01.py

--- LTP2/Pratica08/programa01.py
+++ LTP2/Pratica08/programa01.py
@@ -12,3 +12,7 @@ while continuar == True:
    gastos[categoria] = valor_do_gasto
   print(gastos)
   continuar = input("Deseja continuar? ").lower()  == "sim"
+
+#Passa por todas as categorias e exibe todos os gastos
+for categoria in gastos.keys():
+  print("Categoria:", categoria, "Valor:", gastos[categoria])
