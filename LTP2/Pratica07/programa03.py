6c084ae1b9cf9c2885214a0196a7afbbf339e551
vmedej <70210745+vmedej@users.noreply.github.com>
2020-11-25 22:28:22 -0300

    Update programa03.py

--- LTP2/Pratica07/programa03.py
+++ LTP2/Pratica07/programa03.py
@@ -1,3 +1,4 @@
+  
 #Leitura dos dados
 nome = input("Informe um nome: ")
 poder = input("Informe um poder: ")
@@ -7,5 +8,13 @@ universo = input("Informe qual é o universo: ")
 #Possibilidade 1
 heroi = {"nome": nome, "poder": poder, "historia_origem": historia_origem, "universo": universo}
 
-print("Criado pelo método 1: ")
-print(heroi)
+print("Criado pelo método 1:", heroi)
+
+#Possibilidade 2
+heroi2 = {}
+heroi2["nome"] = nome
+heroi2["poder"] = poder
+heroi2["historia_origem"] = historia_origem
+heroi2["universo"] = universo
+
+print("Criado pelo método 2:", heroi2)
