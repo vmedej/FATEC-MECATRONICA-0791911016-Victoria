2ca1010125d9c7252bf01d7022f753db9ca34cb3
vmedej <70210745+vmedej@users.noreply.github.com>
2020-11-27 19:55:19 -0300

    Create programa03.py

---
+++ LTP1/Pratica07/programa03.py
@@ -0,0 +1,35 @@
+#Calculadora personalisada
+#1 - Soma 
+#2 - Subtração 
+#3 - Multiplicação 
+#4 - Divisão
+
+def exibirMenu():
+  print("Menu Calculadora")
+  print("1 - somar")
+  print("2 - subtrair")
+  print("3 - multiplicar")
+  print("4 - dividir")
+  print ("0 - sair")
+
+def somar(numero1, numero2):
+  return numero1 + numero2
+
+def subtrair(numero1, numero2):
+  return numero1 - numero2
+
+#Programa principal
+continuar = True
+while continuar == True:
+  exibirMenu()
+  opcao = int(input("Digite a opção escolhida: "))
+  if opcao == 1:
+    n1 = float(input("Número 1: "))
+    n2 = float(input("Número 2: "))
+    print("Resultado:", somar(n1,n2))
+  elif opcao == 2:
+    n1 = float(input("Número 1: "))
+    n2 = float(input("Número 2: "))
+    print("Resultado:", subtrair(n1,n2))
+  elif opcao == 0:
+    continuar = False
